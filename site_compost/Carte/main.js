c760f9bab6e4b632d20877c20a11cd5bd6470f70
Shelssiah <shelmyassiah@gmail.com>
2023-04-06 22:36:35 +0200

    envoie

--- site_compost/Carte/main.js
+++ site_compost/Carte/main.js
@@ -1,32 +1,125 @@
 
-var map = L.map('map');
-map.setView([46.63728, 2.3382623], 6);
-
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+var baseLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
             {
                 attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href = "https://carto.com/attributions"> CARTO</a>', //paramètre pour ajouter le nom du fournisseur des tuiles
                 zmaxZoom: 17, //rester aux alentours de vue
-                minZoom: 5,
-                zoomControl: false
-            }) 
-            .addTo(map);
-    $.ajax({
-    url: "resources/data/vilaine.geojson",
+                minZoom: 5
+            });
+            
+var map = L.map('map', {
+    zoomControl : false,
+    layers : [baseLayer]
+});
+map.setView([46.63728, 2.3382623], 6);
+
+var info = L.control();
+info.onAdd = function (map){
+    this._div = L.DomUtil.create('div','info');
+    this.update();
+    return this._div;
+};
+info.update = function (props) {
+    this._div.innerHTML = '<h2>Quantité de biodéchets</h2>' + (props ?
+        '<br/><b>' + props.nom + '</b><br/>' + props.biodechets : '<br/> Sélectionner un région ou une collectivité');
+};
+
+info.addTo(map);
+
+var legend = L.control({position : 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    var grades = [0, 20,40,60,80,100];
+    var labels = [];
+    var from, to;
+
+    for (var i = 0; i< grades.length; i++) {
+        from = grades[i];
+        to =grades[i+1];
+        console.log(getColor(to));
+
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"> </i>'+
+            from + (to ? '&ndash;' + to + 'kg/an/H' : ' + kg/an/H')
+        );
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+}
+
+legend.addTo(map);
+
+function getColor(d) {
+    return d > 100 ? '#993404' :
+            d > 80 ? '#d95f0e' :
+            d > 60 ? '#fe9929' :
+            d > 40 ? '#fec44f' :
+            d > 20 ? '#fee391' :
+            '#ffffd4';
+}
+
+function style(feature) {
+    return {
+        color: "grey", //Couleur de la bordure du polygone(ici rouge)
+        weight: 1, //Epaisseur de la bordure(ici rouge)
+        opacity: 1,
+        dashArray: '',
+        fillColor: getColor(feature.properties.biodechets), //Couleur de l’intérieur du polygone(ici rouge)
+        fillOpacity: 0.9 
+    };
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: hightlightFeature,
+        mouseout: resetHighlight,
+        click:zoomToFeature
+    });
+    layer.bindPopup( "<strong>" + feature.properties.nom + "</strong><br/>" + "Quantité de biodéchets :  " + feature.properties.biodechets);
+}
+
+function hightlightFeature(e){
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 3,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    //info.update(layer.feauture.properties);
+}
+
+
+function resetHighlight(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 1,
+        color: 'grey',
+        dashArray: '',
+        fillOpacity: 0.9
+    });
+}
+
+function zoomToFeature(e){
+    map.fitBounds(e.target.getBounds());
+}
+$.ajax({
+    url: "resources/data/carte-chaleur.geojson",
     dataType: "json",
     success: function (vilaine) {
         
-        L.geoJson(vilaine,{
-            style: function (feature) {
-                return {
-                    color: "#999999", //Couleur de la bordure du polygone(ici rouge)
-                    weight: 1, //Epaisseur de la bordure(ici rouge)
-                    fillColor: d3.interpolateYlOrRd(feature.properties.densite / 150.94), //Couleur de l’intérieur du polygone(ici rouge)
-                    fillOpacity: 0.6 //Opacité de l’intérieur du polygone
-                };
-            },
-            onEachFeature: function (feature, layer) {
-                layer.bindPopup( "<strong>" + feature.properties.nom + "</strong><br/>" + "Niveau de densité : " + feature.properties.densite );
-            }
+        var geojson = L.geoJson(vilaine,{
+            style: style,
+            onEachFeature: onEachFeature 
+                //layer.bindPopup( "<strong>" + feature.properties.nom + "</strong><br/>" + "Quantité de biodéchets :  " + feature.properties.biodechets);
         }).addTo(map);
     }
 });
